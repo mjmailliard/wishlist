d2a7701c47fc82b3940158f788e749f7542ec003
Mike <mjmailliard@gmail.com>
2018-12-14 14:09:54 -0700

    added pw verification against db

--- wishlist_ui/src/components/Login.js
+++ wishlist_ui/src/components/Login.js
@@ -2,20 +2,20 @@ import React, { Component } from 'react';
 import Layout from './Layout';
 import './Login.css'
 
-  // <h2>Login Form</h2>
-  // <p>Email here</p>
-  // <p>Password here</p>
-  // <a href="/"><button>Forgot Password</button></a>  
-  // <a href="/"><button>Login</button></a>
-  
+
+
+
   class Login extends Component {
     constructor(props) {
       super(props);
       this.state = {
         email: '',
-        password: ''
+        password: '',
+        dbName: '',
+        db_id: '',
+        dbPassword: ''
       };
-  
+
       this.handleChange = this.handleChange.bind(this);
       this.handleSubmit = this.handleSubmit.bind(this);
     }
@@ -23,24 +23,46 @@ import './Login.css'
     handleChange(event) {
       this.setState({});
     }
-  
+    
     handleSubmit(event) {
     event.preventDefault()
-      const formData = JSON.stringify({...this.state})
-       fetch('http://localhost:3050/users',  {
-      method: 'POST',
-      body: formData,
+      // const formData = JSON.stringify({...this.state})
+
+       fetch(`http://localhost:3050/users/email/${this.state.email}`, {
       headers: {
         "Content-Type":"application/json"
       }
-    })
-  
-      console.log(formData)
-      this.setState = {
+    }).then(results => {
+     
+       return results.json()
+     
+    }).then(data => {
+
+      const db_data = data.reduce((acc, cur) => cur, 0)
+
+
+     this.setState({
+     
+       dbName: db_data.name,
+       db_id: db_data._id,
+       dbPassword: db_data.password
+ 
+     }, () => {
+       if ( this.state.password === this.state.dbPassword) {
+// redirect here
+       }else {
+        alert('you entered an incorrect password')
+        }
        
-        email: '',
-        password: ''
-      };
+       
+      })
+ 
+
+    })
+
+  
+     
+
     
     }
     
@@ -53,10 +75,11 @@ import './Login.css'
               <label>Email: </label><br/>
                 <input type='email' name='email' onChange={event => this.setState({email: event.target.value})} id='emailInput' placeholder='email@email.com'/><br/>
               <label> Password: </label><br/>
-                <input type="password" name="password" onChange={event => this.setState({password: event.target.value})}id='passwordInput' placeholder='password' autoComplete='password'/><br/>
+                <input type="password" name="password" onChange={event => this.setState({password: event.target.value})} id='passwordInput' placeholder='password' autoComplete='password'/><br/>
 
               <input type="submit" value="Submit" />
             </form>
+          
   
           </div>
           </Layout>
